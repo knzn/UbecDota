0e07b7d3b7d1870aa0927dd4b3b94d13da3d086f
knzn <rdacles@gmail.com>
2020-01-16 22:15:40 +0800

    Updates

---
+++ precache-manifest.25895f07ba71ecc2e1182c4ff12acfe3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ac59bf93f6b52d3db00722a8d46d43",
+    "url": "/UbecDota/index.html"
+  },
+  {
+    "revision": "f4bf067cb48d494e5d1d",
+    "url": "/UbecDota/static/css/2.8c1a6027.chunk.css"
+  },
+  {
+    "revision": "1467d4f9109b121f604a",
+    "url": "/UbecDota/static/css/main.d3b9f4d2.chunk.css"
+  },
+  {
+    "revision": "f4bf067cb48d494e5d1d",
+    "url": "/UbecDota/static/js/2.59d67a19.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/UbecDota/static/js/2.59d67a19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1467d4f9109b121f604a",
+    "url": "/UbecDota/static/js/main.18895515.chunk.js"
+  },
+  {
+    "revision": "bbfe36145933d57dd8de",
+    "url": "/UbecDota/static/js/runtime-main.eb91a771.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/UbecDota/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/UbecDota/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/UbecDota/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/UbecDota/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/UbecDota/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/UbecDota/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/UbecDota/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/UbecDota/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/UbecDota/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/UbecDota/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/UbecDota/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/UbecDota/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/UbecDota/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/UbecDota/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/UbecDota/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/UbecDota/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
